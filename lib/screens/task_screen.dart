1cde71353b555e547264bf6dd48dd60eb1013a07
chris-eminence <emichris380@gmail.com>
2022-11-05 00:56:18 +0100

    task screen

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -6,16 +6,50 @@ class TaskScreens extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
-        child: Column(
-          children: [
-            CircleAvatar(child: Icon(Icons.list)),
-            Text('Taskeet'),
-            
-          ],
+      backgroundColor: Colors.purple,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children : [ Container(
+          padding: const EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+              children:  const [
+              CircleAvatar(child: Icon(Icons.list, color: Colors.purple,),
+              backgroundColor: Colors.white,
+          
+              ),
+              SizedBox(height: 10.0,),
+              Text('Taskeet',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700 
+              ),),
+      
+              Text('12 Task',
+              style: TextStyle(
+              color: Colors.white,
+              fontSize: 18.0,
+              ),
+      
+              ),
+              
+              
+            ],
+          ),
         ),
+        Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(topRight: Radius.circular(20.0), topLeft: Radius.circular(20.0))
+                    
+                  ),
+              
+                ),
+              )
+      
+        ],
       ),
     );
   }
