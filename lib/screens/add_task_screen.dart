4fb23c81e7f672f8dc7fbe0ae7c74945761b9846
chris-eminence <emichris380@gmail.com>
2022-11-06 16:11:27 +0100

    BottomSheet implemented

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -7,45 +7,47 @@ class AddTaskScreen extends StatelessWidget {
     return Container(
       color: const Color(0xFF757575),
       child: Container(
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20.0),
-              topRight: Radius.circular(20.0),
-            ),
+        padding: const EdgeInsets.all(35.0),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Text(
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Add task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.purple,
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            const TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.purple),
+                ),
+              ),
+            ),
+            FlatButton(
+              color: Colors.purple,
+              onPressed: () {},
+              child: const Text(
                 'Add task',
-                textAlign: TextAlign.center,
                 style: TextStyle(
-                    color: Colors.purple,
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.bold),
-              ),
-              const TextField(
-                autofocus: true,
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(
-                  border: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.purple),
-                  ),
+                  color: Colors.white,
                 ),
               ),
-              FlatButton(
-                color: Colors.purple,
-                onPressed: () {},
-                child: const Text(
-                  'Add task',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              )
-            ],
-          )),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
