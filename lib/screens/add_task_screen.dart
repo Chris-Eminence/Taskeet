a52a5625811a2a78f25e6d156b856bb7355354f1
chris-eminence <emichris380@gmail.com>
2022-11-06 16:08:13 +0100

    BottomSheet implemented

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFF757575),
+      child: Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Add task',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.purple,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              const TextField(
+                autofocus: true,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.purple),
+                  ),
+                ),
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {},
+                child: const Text(
+                  'Add task',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            ],
+          )),
+    );
+  }
+}
